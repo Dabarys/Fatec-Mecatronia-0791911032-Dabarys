cd506340ef615d52d9800cdf7627dbec387260d7
Dabarys <69904646+Dabarys@users.noreply.github.com>
2020-08-26 08:53:10 -0300

    Update Programa02.c

--- Microcontroladores-Praticas/Pratica02/Programa02.c
+++ Microcontroladores-Praticas/Pratica02/Programa02.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 int main(void) {
   float a,b,x;
   printf("Informe os valores de a e b: ");
   scanf("%f%f", &a, &b);
   x=-b/a;
-  printf("Resultado: %.3f\n", x);
+  printf("Resultado da equação de primeiro grau: %.2f\n", x);
   return 0;
 }
